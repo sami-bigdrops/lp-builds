16ccf346a05a0b3c190366912b92510b6ab0f632
sami-bigdrops <sami@bigdropsmarketing.com>
2026-08-24 21:45:19 +0530

    Vercel Build Ignore Script

---
+++ apps/web/vercel-ignore-build.sh
@@ -0,0 +1,9 @@
+echo "Branch: $VERCEL_GIT_COMMIT_REF"
+
+if [ "$VERCEL_GIT_COMMIT_REF" = "main" ] || [ "$VERCEL_GIT_COMMIT_REF" = "pre-prod" ]; then
+  echo "Build allowed"
+  exit 1
+fi
+
+echo "Build ignored"
+exit 0
